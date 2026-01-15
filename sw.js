2b3ef313593f30532feb5e51774146f884eb74e4
luisgalpa-code <luisgalpa@gmail.com>
2026-01-15 19:58:58 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "gastos-fijos-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+// Cache-first (rápido y offline)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
